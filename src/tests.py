Fixed complementary enthalpy calculation & added check for the energy balance in each cell of BPHE
Signed-off-by: Ian Bell <ian.h.bell@gmail.com>

src/tests.py
@@ -10,14 +10,11 @@ iQ = ACHP.iQ
 B = ACHP.BrazedPlateHeatExchanger()
 
 fig = plt.figure(figsize=(6,3))
-ax1 = fig.add_subplot(121)
-ax2 = fig.add_subplot(122)
-ax1.set_yscale('log')
 
-for mdot_ratio_h_c in np.logspace(-2,2):
+for mdot_ratio_h_c in np.logspace(np.log10(0.1),np.log10(5),200):
     # Cold stream inlet
     B.State_c_inlet = ACHP.CoolPropStateClassSI("Propane")
-    B.State_c_inlet.update(iT,290,iQ,1)
+    B.State_c_inlet.update(iT,290,iQ,0.3)
     B.State_c_inlet.update(iT,270,iP,B.State_c_inlet.p())
     B.mdot_c = 0.01
     
@@ -28,27 +25,33 @@ for mdot_ratio_h_c in np.logspace(-2,2):
     B.mdot_h = B.mdot_c*mdot_ratio_h_c
     
     B.SaturationStates()
-    B.DetermineQmax()
+    Qmax = B.DetermineQmax()
     
     ph = [B.State_h_inlet.p()/1000 for i in range(len(B.EnthalpyList_h))]
     pc = [B.State_c_inlet.p()/1000 for i in range(len(B.EnthalpyList_h))]
-    hh = [h/1000 for h in B.EnthalpyList_h]
-    hc = [h/1000 for h in B.EnthalpyList_c]
-    hhnorm = (np.array(hh)-hh[0])/(hh[-1]-hh[0])
-    hcnorm = (np.array(hc)-hc[0])/(hc[-1]-hc[0])
+    hh = np.array([h/1000 for h in B.EnthalpyList_h])
+    hc = np.array([h/1000 for h in B.EnthalpyList_c])
+    hhnorm = B.mdot_h*(np.array(hh)-hh[0])/Qmax*1000
+    hcnorm = B.mdot_c*(np.array(hc)-hc[0])/Qmax*1000
     Th = [T for T in B.TemperatureList_h]
     Tc = [T for T in B.TemperatureList_c]
     
+    ax1 = fig.add_subplot(121)
+    ax2 = fig.add_subplot(122)
+    ax1.set_yscale('log')
+
     Ph('Propane',axis = ax1)
     ax1.plot(hh, ph, 'ro-')
     ax1.plot(hc, pc, 'bo-')
     ax1.set_title('')
     ax1.set_ylim(ymin = 100)
     
-    ax2.plot(hcnorm,Tc,'bo-')
-    ax2.plot(hcnorm,Th,'ro-')
+    ax2.plot(hcnorm, Tc, 'bo-')
+    ax2.plot(hhnorm, Th, 'ro-')
+    ax2.set_xlim(0,1)
+    
+    plt.suptitle(str(mdot_ratio_h_c))
     
     plt.tight_layout()    
     plt.savefig('{s:08d}'.format(s=int(mdot_ratio_h_c*1000))+'.png')    
-    ax1.cla()
-    ax2.cla()
+    fig.clf()